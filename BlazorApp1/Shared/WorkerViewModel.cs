9830e4b8c4c6a1e97ec5d7687f58bf678d738201
Juan Araica <juanaraica149@gmail.com>
2022-08-13 17:04:28 -0600

    Actualizacion y agregacion de interfaces

--- BlazorApp1/Shared/WorkerViewModel.cs
+++ BlazorApp1/Shared/WorkerViewModel.cs
@@ -10,7 +10,7 @@ namespace BlazorApp1.Shared
     public class WorkerViewModel
     {
 
-        public string Id { get; set; }
+        public int Id { get; set; }
         [Required]
         [Display(Name = "ID ")]
         public string CedulaTra { get; set; }
